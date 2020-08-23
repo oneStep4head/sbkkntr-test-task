6252907db993a439d1f7df3edd43e635b2c721e8
oneStep4head <c1o2w3b4>
2020-08-23 19:03:51 +0700

    - :<

--- client.js
+++ client.js
@@ -1,7 +1,77 @@
 const { axios } = require("./fakeBackend/mock");
 
-const getFeedbackByProductViewData = async (product, actualize = false) => {
+const userIdSeparator = ',';
 
+const getUsersById = () => {
+    let result = {};
+}
+
+const getUniqueValuesFromArray = (arr) => {
+    let result = {};
+
+    arr.forEach(item => {
+        result[item] = true;
+    });
+
+    return Object.keys(result);
+}
+
+const apiConfig = {
+    users: (ids) => {
+        let apiRoute = '/users';
+        const hasParams = ids && ids.length;
+        const apiRouteParam = hasParams && ids.length > 1 ? '?ids=' : '?id=';
+
+        if (!hasParams) {
+            return apiRoute;
+        }
+
+        apiRoute = apiRoute.concat(apiRouteParam);
+
+        return apiRoute.concat(ids.join(userIdSeparator));
+    },
+
+    feedback: (productId) => {
+        const apiRoute = '/feedback?product=';
+
+        return `${apiRoute}${productId}`;
+    }
+}
+
+const getFeedbackByProductViewData = async (product, actualize = false) => {
+    return axios(apiConfig.feedback(product))
+        .then(response => {
+            if (response && response.status === 200) {
+                if (response.data.feedback) {
+                    const userIds = getUniqueValuesFromArray(response.data.feedback.map(f => f.userId));
+
+                    axios(apiConfig.users(userIds))
+                        .then(response => {
+                            if(response && response.status === 200) {
+                                if (response.data.users) {
+                                    const usersMap = {}
+                                    response.data.users.forEach(u => {
+                                        usersMap[u.id] = u;
+                                    });
+
+                                    return usersMap;
+                                }
+                            }
+                        })
+                        .then(usersMap => {
+                            if (isersMap) {
+
+                            }
+                        });
+
+                    return response.data;
+                }
+
+            return response.data;
+        } else {
+            return response.message;
+        }
+    });
 };
 
 module.exports = { getFeedbackByProductViewData };
